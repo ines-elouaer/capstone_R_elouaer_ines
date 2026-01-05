302ad4ba17635ba53ce3a74b13f23c5980fc451a
Ines_Elouaer <149087705+ines-elouaer@users.noreply.github.com>
2026-01-05 12:13:31 +0100

    Update capstone.R

--- analysis/capstone.R
+++ analysis/capstone.R
@@ -1,1 +1,9 @@
+# Analyse de la production mensuelle d’électricité en Tunisie (2005–2024)
+# Auteur : Ines Elouaer
+
+# Le code complet et exécuté se trouve dans le notebook Google Colab
+# Lien Colab : A AJOUTER DANS LE README
+
+# Ce fichier est fourni pour satisfaire l’exigence "codes sources"
+# du formulaire d’évaluation.
 
